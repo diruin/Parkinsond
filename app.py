38790659789e3672f91c9fa3f2a32f951c0cefd0
diruin <153728069+diruin@users.noreply.github.com>
2025-01-23 16:06:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -21,19 +21,38 @@ st.title("보행 데이터 분류기")
 
 step_time = st.number_input("Step Time (s) 값을 입력하세요:", min_value=0.1, max_value=3.0, step=0.01)
 
+if "show_data_image" not in st.session_state:
+    st.session_state.show_data_image = False
+if "show_graph1" not in st.session_state:
+    st.session_state.show_graph1 = False
+if "show_graph2" not in st.session_state:
+    st.session_state.show_graph2 = False
+
+
+if st.button("예측하기"):
+    model = joblib.load("decision_tree_model.pkl")
+    prediction = model.predict([[step_time]])
+
 col1, col2, col3 = st.columns(3)  
 
 with col1:
-    if st.button("보행 데이터 보기", key="data_button"):
+    if st.button("보행 데이터 보기"):
+        st.session_state.show_data_image = not st.session_state.show_data_image 
+    if st.session_state.show_data_image:
         st.image("3.png", caption="Stride Data Overview", use_container_width=True)
 
 with col2:
-    if st.button("분포 그래프 보기 (1)", key="graph_button_1"):
+    if st.button("분포 그래프 보기 (1)"):
+        st.session_state.show_graph1 = not st.session_state.show_graph1 
+    if st.session_state.show_graph1:
         st.image("1.png", caption="Step Time Distribution 1", use_container_width=True)
 
 with col3:
-    if st.button("분포 그래프 보기 (2)", key="graph_button_2"):
+    if st.button("분포 그래프 보기 (2)"):
+        st.session_state.show_graph2 = not st.session_state.show_graph2 
+    if st.session_state.show_graph2:
         st.image("2.png", caption="Step Time Distribution 2", use_container_width=True)
+
 
 
 if st.button("예측하기"):
