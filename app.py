7ce7cc05930c21bb344de3ddb4648f0ea79d2e23
diruin <153728069+diruin@users.noreply.github.com>
2025-02-03 10:30:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ st.markdown(
     """
     <style>
     body {
-        background-color: #B90000 !important; /* 배경색 */
+        background-color: #8B4513 !important; /* 배경색 */
     }
     
     .stApp {
